2274246d67461b8b75f16d025d48fcc06fa044ff
OnikuSpedu <srakibnyc1@gmail.com>
2021-01-13 19:59:10 -0500

    Finished radixSimpleSort and tests for it.

--- Radix.java
+++ Radix.java
@@ -12,7 +12,7 @@ public class Radix {
         }
     }
 
-    public static void merge( SortableLinkedList original, SortableLinkedList[]buckets) {
+    public static void merge(SortableLinkedList original, SortableLinkedList[] buckets) {
         for (int i = 0; i < buckets.length; i++) {
             original.extend(buckets[i]);
         }
@@ -21,41 +21,47 @@ public class Radix {
     public static void radixSortSimple(SortableLinkedList data) {
         if (data.size() > 1) {
             int maxLength = length(getMax(data));
-            for (int i = 0; i < maxLength;  i++) {
-
-                //Init buckets
-                SortableLinkedList[]buckets = new SortableLinkedList[10];
-                for (int j = 0; j < buckets.length; j++) {
-                    buckets[j] = new SortableLinkedList();
-                }
-
-                //Move to buckets
-                for (int j = 0; j < data.size(); j++) {
-                    int n = data.get(0);
-                    data.remove(0);
-                    int digit = nth(n, i);
-                    buckets[digit].add(n);
-                }
-
-                //Merge 
-                merge(data, buckets);
-
+            for (int i = 0; i < maxLength; i++) {
+                sortOnce(data, i);
             }
         }
     }
 
-    public static int getMax(SortableLinkedList data) {
+    private static void sortOnce(SortableLinkedList data, int i) {
+        // Init buckets
+        SortableLinkedList[] buckets = new SortableLinkedList[10];
+        for (int j = 0; j < buckets.length; j++) {
+            buckets[j] = new SortableLinkedList();
+        }
+
+        // Move to buckets
+        int size = data.size();
+        for (int j = 0; j < size; j++) {
+            int n = data.get(0);
+            data.remove(0);
+            int digit = nth(n, i);
+            buckets[digit].add(n);
+        }
+
+        // Merge
+        merge(data, buckets);
+    }
+
+    private static int getMax(SortableLinkedList data) {
         if (data.size() > 0) {
             int max = data.get(0);
             data.remove(0);
             data.add(0);
             for (int i = 0; i < data.size() - 1; i++) {
                 int temp = data.get(0);
-                if (temp > max) { max = temp; }
+                if (temp > max) {
+                    max = temp;
+                }
                 data.remove(0);
                 data.add(temp);
             }
             return max;
-        } else throw new IllegalArgumentException("Data size is less than 1");
+        } else
+            throw new IllegalArgumentException("Data size is less than 1");
     }
 }
