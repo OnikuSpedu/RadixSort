b5d8795f7719c9f9555d82d1b836be1f7c5c48e4
OnikuSpedu <srakibnyc1@gmail.com>
2021-01-13 20:54:03 -0500

    Finished radixSort and fixed bugs

--- Radix.java
+++ Radix.java
@@ -18,13 +18,57 @@ public class Radix {
         }
     }
 
+    public static void mergeNeg(SortableLinkedList original, SortableLinkedList[] buckets) {
+        for (int i = buckets.length - 1; i >= 0; i--) {
+            System.out.println(buckets[i]);
+            original.extend(buckets[i]);
+        }
+    }
+
     public static void radixSortSimple(SortableLinkedList data) {
         if (data.size() > 1) {
-            int maxLength = length(getMax(data));
+            int maxLength = getMaxLength(data);
             for (int i = 0; i < maxLength; i++) {
                 sortOnce(data, i);
             }
         }
+    }
+
+    public static void radixSortNeg(SortableLinkedList data) {
+        if (data.size() > 1) {
+            int maxLength = getMaxLength(data);
+            for (int i = 0; i < maxLength; i++) {
+                sortOnceNeg(data, i);
+            }
+        }
+    }
+
+    public static void radixSort(SortableLinkedList data) {
+        //The goal will be to split into two linked lists. 
+        //One for nonnegative other for positive. Sort them. 
+        //Then merge at the end.
+
+        SortableLinkedList nonNegative = new SortableLinkedList();
+        SortableLinkedList negative = new SortableLinkedList();
+
+        int size = data.size();
+        for (int i = 0; i < size; i++) {
+            int n = data.get(0);
+            data.remove(0);
+            
+            if (n >= 0) {
+                nonNegative.add(n);
+            } else {
+                negative.add(n);
+            }
+        }
+
+        Radix.radixSortSimple(nonNegative);
+        Radix.radixSortNeg(negative);
+
+        data.extend(negative);
+        data.extend(nonNegative);
+
     }
 
     private static void sortOnce(SortableLinkedList data, int i) {
@@ -47,20 +91,41 @@ public class Radix {
         merge(data, buckets);
     }
 
-    private static int getMax(SortableLinkedList data) {
-        if (data.size() > 0) {
-            int max = data.get(0);
+    private static void sortOnceNeg(SortableLinkedList data, int i) {
+        // Init buckets
+        SortableLinkedList[] buckets = new SortableLinkedList[10];
+        for (int j = 0; j < buckets.length; j++) {
+            buckets[j] = new SortableLinkedList();
+        }
+
+        // Move to buckets
+        int size = data.size();
+        for (int j = 0; j < size; j++) {
+            int n = data.get(0);
             data.remove(0);
-            data.add(0);
+            int digit = nth(n, i);
+            buckets[digit].add(n);
+        }
+
+        // Merge
+        mergeNeg(data, buckets);
+    }
+
+    private static int getMaxLength(SortableLinkedList data) {
+        if (data.size() > 0) {
+            int temp = data.get(0);
+            int maxLen = length(temp);
+            data.remove(0);
+            data.add(temp);
             for (int i = 0; i < data.size() - 1; i++) {
-                int temp = data.get(0);
-                if (temp > max) {
-                    max = temp;
+                temp = data.get(0);
+                if (length(temp) > maxLen) {
+                    maxLen = length(temp);
                 }
                 data.remove(0);
                 data.add(temp);
             }
-            return max;
+            return maxLen;
         } else
             throw new IllegalArgumentException("Data size is less than 1");
     }
